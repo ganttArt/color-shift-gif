5639183bc8ae5b4d848ad8d0a0661a2352d310fb
Christopher Gantt <gantt.art@gmail.com>
2023-01-13 08:37:28 -1000

    Open and show the image selected

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from PIL import Image
 
+IMAGE = "test.png"
 
 def main():
-    print('hello world')
-
+    with Image.open(IMAGE) as image:
+        try:
+            if image.format != 'JPEG':
+                image = image.convert('RGB')
+        except Exception:
+            print('File type not compatible')
+            return
+        
+        image.show()
 
 if __name__ == '__main__':
     main()
