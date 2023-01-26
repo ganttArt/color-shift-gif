7f13874b968db72876180a6aeb7e712aa6171762
Christopher Gantt <gantt.art@gmail.com>
2023-01-26 09:18:31 -1000

    Include command line argument for indicating transparency

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ parser.add_argument('-nl', '--noloop', action='store_const',
                     const=True, default=False, help='turn off looping in the GIF')
 parser.add_argument('-p', '--posterize', action='store_const',
                     const=True, default=False, help='posterize setting')
+parser.add_argument('-t', '--transparent', action='store_const',
+                    const=True, default=False, help='maintain transparency in image')
 args = parser.parse_args()
 
 
 def main():
     try:
         with Image.open(IMAGE) as image:
-            if image.format != 'JPEG' and not args.posterize:
+            if image.format != 'JPEG' and not args.posterize and not args.transparent:
                 image = image.convert('RGB')
             create_gif(image, noloop=args.noloop, posterize=args.posterize, filename=IMAGE)
     except UnidentifiedImageError:
